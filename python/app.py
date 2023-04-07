63f873d9cd5b34435214d4ba3f72cb2470f80b7b
ThingEngineer <josh.lee.campbell@gmail.com>
2023-04-06 21:34:51 -0500

    add header
    distribution and licence info header added

--- python/app.py
+++ python/app.py
@@ -1,3 +1,31 @@
+#
+# app.py
+#
+#      __  __  _____ _     _             _____            _                      
+#     / / / / |_   _| |   (_)           |  ___|          (_)                     
+#    / / / /    | | | |__  _ _ __   __ _| |__ _ __   __ _ _ _ __   ___  ___ _ __ 
+#   / / / /     | | | '_ \| | '_ \ / _` |  __| '_ \ / _` | | '_ \ / _ \/ _ \ '__|
+#  / / / /      | | | | | | | | | | (_| | |__| | | | (_| | | | | |  __/  __/ |   
+# /_/ /_/       \_/ |_| |_|_|_| |_|\__, \____/_| |_|\__, |_|_| |_|\___|\___|_|   
+#                                   __/ |            __/ |                       
+#                                  |___/            |___/ 
+#
+# This file is part of the 'Basic Microcontroller API Consumption' distribution (https://github.com/ThingEngineer/Basic-IoT-API).
+# Copyright (c) 2023 Josh Campbell (https://thing.engineer)
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# 
+#/
 from flask import Flask, request, jsonify, render_template
 import sqlite3
 from datetime import datetime
